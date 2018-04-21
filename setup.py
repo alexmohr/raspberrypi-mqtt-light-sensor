eb700b962a8cd2e84cc02d55a0ab140cf87c2fa9
Alex <git@mohr.io>
2018-04-21 22:17:50 +0200

    Added commandline options for configuraiton. Added descrritption to pypy

--- setup.py
+++ setup.py
@@ -3,27 +3,35 @@
 from __future__ import absolute_import
 import sys
 import os
-from setuptools import setup, find_packages
+from setuptools import setup
 # import subprocess
 sys.path.insert(0, '.')
 
 CURRENT_DIR = os.path.dirname(__file__)
 
-# to deploy to pip, please use
-# make pythonpack
+with open('README.md', 'r') as myfile:
+    long_description = myfile.read().replace('\n', '')
+
+
 # python setup.py register sdist upload
-# and be sure to test it firstly using "python setup.py register sdist upload -r pypitest"
+# and be sure to test it firstly using "python setup.py register sdist
+# upload -r pypitest"
 setup(name='raspberrypi-mqtt-light-sensor',
-  packages = ['raspberrypi-mqtt-light-sensor'], # this must be the same as the name above
-  version = '0.1.6',
-  description = 'Reading a LDR with a raspberry pi and publishing the data via mqtt',
-  author = 'Alexander Mohr',
-  author_email = 'pypy@mohr.io',
-  url = 'https://github.com/alexmohr/raspberrypi-mqtt-light-sensor', # use the URL to the github repo
-  download_url = 'https://codeload.github.com/alexmohr/raspberrypi-mqtt-light-sensor/tar.gz/0.1.6',
-  keywords = ['mqtt', 'rpi', 'raspberypi', 'light', 'sensor'], 
-  classifiers = [],
-  install_requires=[
-      'paho-mqtt'
-  ],
-)
+      # this must be the same as the name above
+      packages=['raspberrypi-mqtt-light-sensor'],
+      version='0.2.0',
+      description='Reading a LDR with a raspberry pi and publishing the data via mqtt',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      author='Alexander Mohr',
+      author_email='pypy@mohr.io',
+      # use the URL to the github repo
+      url='https://github.com/alexmohr/raspberrypi-mqtt-light-sensor',
+      download_url='https://codeload.github.com/alexmohr/raspberrypi-mqtt-light-sensor/tar.gz/0.2.0',
+      keywords=['mqtt', 'rpi', 'raspberypi', 'light', 'sensor'],
+      license='MIT',
+      classifiers=[],
+      install_requires=[
+          'paho-mqtt'
+      ],
+      )
